23906e6783e8b75fc73b339bbf9527f1408fc902
Elozzy <st.eloghosa@gmail.com>
2019-01-22 11:44:31 +0100

    Added calulations code so box would appear on the face

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import './App.css';
 
 // the clarifai api init
 const app = new Clarifai.App({
-  apiKey: 'API KEY HERE'
+  apiKey: 'a411c92e3db74903b4d925eac40fb6a4'
  });
 
 
@@ -39,6 +39,19 @@ class App extends Component {
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol : width - (clarifaiFace.right_col * width),
+      bottomRow : height - ( clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({box: box});
   }
 
 
@@ -51,7 +64,7 @@ class App extends Component {
     app.models.predict(
       Clarifai.FACE_DETECT_MODEL, 
       this.state.input)
-    .then(response => this.calculateFaceLocation(response))
+    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
     .catch(err => console.log(err));
   }
 
@@ -69,7 +82,7 @@ class App extends Component {
         onInputChange={this.onInputChange} 
         onButtonSubmit={this.onButtonSubmit}
          />
-         <FaceRecognition imageUrl={this.state.imageUrl} />
+         <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
 
         </div>
     );
