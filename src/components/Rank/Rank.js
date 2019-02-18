1587fa381c8c08c6f0add7d44576bbbf55140969
Elozzy <st.eloghosa@gmail.com>
2019-02-18 16:52:23 +0100

    added few lines of code to this file

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
-const Rank = () => {
-    return (
-        <div>
-            <div className='white f3'>
-                {'Andreir, your current rank is...'}
-            </div>
-            <div className='white f3'>
-                {'#5'}
-            </div>
-        </div>
-    );
+const Rank = ({ name, entries }) => {
+  return (
+    <div>
+      <div className='white f3'>
+        {`${name}, your current entry count is...`}
+      </div>
+      <div className='white f1'>
+        {entries}
+      </div>
+    </div>
+  );
 }
 
-export default Rank
+export default Rank;
